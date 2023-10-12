29feb18314fe8aa022f4902b4570567327e49669
AfroTidus <115271505+AfroTidus@users.noreply.github.com>
2023-10-12 20:10:43 +0100

    Add files via upload

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButtonAdd = new System.Windows.Forms.RadioButton();
+            this.radioButtonSubtract = new System.Windows.Forms.RadioButton();
+            this.radioButtonMultiply = new System.Windows.Forms.RadioButton();
+            this.radioButtonPower = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "number1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "number2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "result";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 77);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 163);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(213, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(163, 215);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(213, 26);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(613, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Operator";
+            // 
+            // radioButtonAdd
+            // 
+            this.radioButtonAdd.AutoSize = true;
+            this.radioButtonAdd.Location = new System.Drawing.Point(620, 113);
+            this.radioButtonAdd.Name = "radioButtonAdd";
+            this.radioButtonAdd.Size = new System.Drawing.Size(43, 24);
+            this.radioButtonAdd.TabIndex = 12;
+            this.radioButtonAdd.TabStop = true;
+            this.radioButtonAdd.Text = "+";
+            this.radioButtonAdd.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonSubtract
+            // 
+            this.radioButtonSubtract.AutoSize = true;
+            this.radioButtonSubtract.Location = new System.Drawing.Point(620, 143);
+            this.radioButtonSubtract.Name = "radioButtonSubtract";
+            this.radioButtonSubtract.Size = new System.Drawing.Size(39, 24);
+            this.radioButtonSubtract.TabIndex = 13;
+            this.radioButtonSubtract.TabStop = true;
+            this.radioButtonSubtract.Text = "-";
+            this.radioButtonSubtract.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMultiply
+            // 
+            this.radioButtonMultiply.AutoSize = true;
+            this.radioButtonMultiply.Location = new System.Drawing.Point(620, 173);
+            this.radioButtonMultiply.Name = "radioButtonMultiply";
+            this.radioButtonMultiply.Size = new System.Drawing.Size(40, 24);
+            this.radioButtonMultiply.TabIndex = 14;
+            this.radioButtonMultiply.TabStop = true;
+            this.radioButtonMultiply.Text = "*";
+            this.radioButtonMultiply.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonPower
+            // 
+            this.radioButtonPower.AutoSize = true;
+            this.radioButtonPower.Location = new System.Drawing.Point(620, 203);
+            this.radioButtonPower.Name = "radioButtonPower";
+            this.radioButtonPower.Size = new System.Drawing.Size(77, 24);
+            this.radioButtonPower.TabIndex = 15;
+            this.radioButtonPower.TabStop = true;
+            this.radioButtonPower.Text = "power";
+            this.radioButtonPower.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButtonPower);
+            this.Controls.Add(this.radioButtonMultiply);
+            this.Controls.Add(this.radioButtonSubtract);
+            this.Controls.Add(this.radioButtonAdd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButtonAdd;
+        private System.Windows.Forms.RadioButton radioButtonSubtract;
+        private System.Windows.Forms.RadioButton radioButtonMultiply;
+        private System.Windows.Forms.RadioButton radioButtonPower;
+    }
+}
+
